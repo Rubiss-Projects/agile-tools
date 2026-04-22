987614dddc852dbe334840076959f5930131a160
Ben Lawson <rubiss@gmail.com>
2026-04-21 23:04:32 -0400

    Address PR review comments

--- packages/jira-client/src/index.ts
+++ packages/jira-client/src/index.ts
@@ -1,7 +1,7 @@
 export { JiraClient, JiraClientError, createJiraClient } from './client.js';
 export type { JiraServerInfo, FetchOptions } from './client.js';
 
-export { listBoards, getBoardDetail, getBoardFilterId } from './discovery.js';
+export { listBoards, getBoardDetail, getBoardDetailWithFilterId, getBoardFilterId } from './discovery.js';
 
 export { fetchBoardIssues, streamBoardIssues, fetchIssueChangelog, streamJqlIssues } from './issues.js';
 export type {
